0cc3b149eb0a0a6109aac71e0b2f109516e66959
jeancy001 <jeancympoy24@gmail.com>
2026-07-11 15:27:48 +0100

    added

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Onboarding } from '@/components/onboarding'
-
-export default function Page() {
-  return <Onboarding />
-}
--- app/signin/page.tsx
+++
@@ -1,5 +0,0 @@
-import { AuthScreen } from '@/components/auth-screen'
-
-export default function SignInPage() {
-  return <AuthScreen mode="signin" />
-}
